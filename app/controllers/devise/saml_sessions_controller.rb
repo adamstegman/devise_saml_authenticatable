91f65867ea13daef8f5ca8f4f53754ac953d943d
Adam Stegman <astegman@gmail.com>
2015-06-01 15:34:29 -0700

    Skip CSRF from IdP

--- app/controllers/devise/saml_sessions_controller.rb
+++ app/controllers/devise/saml_sessions_controller.rb
@@ -4,6 +4,8 @@ class Devise::SamlSessionsController < Devise::SessionsController
   include DeviseSamlAuthenticatable::SamlConfig
   unloadable if Rails::VERSION::MAJOR < 4
   before_filter :get_saml_config
+  skip_before_action :verify_authenticity_token
+
   def new
     request = OneLogin::RubySaml::Authrequest.new
     action = request.create(@saml_config)
